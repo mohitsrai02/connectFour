1ef749283da150720f67ff226a21020c0cf8c9c5
mohitsrai02 <mohitsrai@live.in>
2021-10-20 19:49:44 +0530

    two

--- connect.cpp
+++ connect.cpp
@@ -2,47 +2,74 @@
 #include <iostream>
 using namespace std;
 void printBoard(int row, int col, string arr[40][40]);
-
+void insertDisk(int row, int col, int rowNum, string player, string arr[40][40]);
 int main()
 {
     int r, c, p;
     string board[40][40];
     cout << "Enter no. of rows and columbs:-" << endl;
     cin >> r >> c;
-    int maxTurns = r*c;
-    int turnCount = 1;
+    int maxTurns = r * c;
+    int turnCount = 0;
+    int insert;
     cout << "Enter win connections required:-" << endl;
     cin >> p;
     //Initializing Row numbers
     for (int i = 1; i <= c; i++)
     {
-             board[0][i]= to_string(i);
+        board[0][i] = to_string(i);
     }
     //Initializing the 2D array with O
     for (int i = 1; i <= r; i++)
     {
         for (int j = 1; j <= c; j++)
         {
-             board[i][j]="O";
+            board[i][j] = "O";
         }
     }
 
-    printBoard(r,c,board);
+    printBoard(r, c, board);
 
-    string player1 = "red";
-    string player2 = "yellow";
-
-    while(1){
+    string player1 = "r";
+    string player2 = "y";
+    //Setting up an Infinite while loop
+    while (1)
+    {
         if (turnCount % 2 != 0)
         {
-            cout<<"Red's Turn"<<endl;
+            turnCount++;
+            cout << endl
+                 << "Red's Turn" << endl
+                 << "Enter Row No. where you would Like to insert your disk : ";
+            cin >> insert;
+            insertDisk(r,c,insert, player2,board);
+            printBoard(r, c, board);
         }
-
-        break;
-
+        else
+        {
+            turnCount++;
+            cout << endl
+                 << "Yellow's Turn" << endl
+                 << "Enter Row No. where you would Like to insert your disk : ";
+            cin >> insert;
+            insertDisk(r,c,insert, player2,board);
+            printBoard(r, c, board);
+        }
+        if (turnCount == maxTurns)
+        {
+            break;
+        }
     }
     return 0;
 }
+//Insert Disk in board Function
+
+void insertDisk(int row, int col, int rowNum, string player, string arr[40][40]){
+    arr[2][2] = player;
+
+}
+
+//Print Board Function
 void printBoard(int row, int col, string arr[40][40])
 {
     for (int i = 0; i <= row; i++)
@@ -51,6 +78,6 @@ void printBoard(int row, int col, string arr[40][40])
         {
             cout << "|" << arr[i][j] << "|";
         }
-        cout <<endl;
+        cout << endl;
     }
 }
